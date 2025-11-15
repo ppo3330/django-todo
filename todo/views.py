aa5a2b90d7087a547978524c06fd2140cf950444
ppo3330 <junseok3330@gmail.com>
2025-11-15 15:50:25 +0900

    feat: Initialize To-Do project with Task model

---
+++ todo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+        
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
