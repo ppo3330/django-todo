44ce0a6e9c30b720921d7493ca57bb6e8a341e16
ppo3330 <junseok3330@gmail.com>
2025-11-17 15:42:46 +0900

    feat: 체크박스 추가

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('toggle/<int:pk>/', views.task_toggle, name='task_toggle'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def task_list(request):
@@ -8,5 +8,11 @@ def task_list(request):
             Task.objects.create(title=title)
         return redirect('task_list')
         
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks = Task.objects.all().order_by('is_completed', '-created_at')
     return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def task_toggle(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect('task_list')
